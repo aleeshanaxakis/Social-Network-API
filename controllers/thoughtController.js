62f60c14831bb4787c156d6f8791c814616ef7d3
Aleesha Naxakis <aleesha-@hotmail.com>
2024-01-05 17:46:52 +1100

    added main index.js file and code

---
+++ controllers/thoughtController.js
@@ -0,0 +1,13 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+  getAllThoughts: async (req, res) => { /* Implementation */ },
+  getThoughtById: async (req, res) => { /* Implementation */ },
+  createThought: async (req, res) => { /* Implementation */ },
+  updateThought: async (req, res) => { /* Implementation */ },
+  deleteThought: async (req, res) => { /* Implementation */ },
+  createReaction: async (req, res) => { /* Implementation */ },
+  deleteReaction: async (req, res) => { /* Implementation */ },
+};
+
+module.exports = thoughtController;
---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const { User } = require('../models');
+
+const userController = {
+  getAllUsers: async (req, res) => { /* Implementation */ },
+  getUserById: async (req, res) => { /* Implementation */ },
+  createUser: async (req, res) => { /* Implementation */ },
+  updateUser: async (req, res) => { /* Implementation */ },
+  deleteUser: async (req, res) => { /* Implementation */ },
+};
+
+module.exports = userController;
